58ae98c0f65e63764469aa08107542e46c60b5c6
980263036@qq.com <980263036@qq.com>
2020-01-04 21:11:08 +0800

    first commit

---
+++ TestSwitch.java
@@ -0,0 +1,15 @@
+public class TestSwitch {
+    public static void main (String[] args) {
+        int i = 10;
+        switch (i)  {
+            case 8 :
+            System.out.println("S");
+            break;
+            case 18 :
+            System.out.println("A");
+            break;
+            default:
+            System.out.println("error");
+        }
+    }
+}
